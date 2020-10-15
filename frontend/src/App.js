b0b802e2f697967fb4a9e21a33b1c66cf95cc865
Wesley <wesaboo@gmail.com>
2020-10-14 21:42:37 -0700

    install react-router-dom, and react-router-bootstrap, change href to Link component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,21 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import ProductContainer from "./pages/ProductContainer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
       <div className="pageBody py-3">
         <Container>
-          <Home />
+          <Route path="/" component={Home} exact />
+          <Route path="/product/:id" component={ProductContainer} />
         </Container>
       </div>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
