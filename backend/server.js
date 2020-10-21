5f64f26e8f0db46cbe3d750e859fd8a35c31b06f
Wesley <wesaboo@gmail.com>
2020-10-20 19:31:33 -0700

    adds routes, errorhandling

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 
@@ -15,13 +16,11 @@ app.get("/", (req, res) => {
   res.send("Backend running!");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+// Fetching from database
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
+// Error Handling
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
