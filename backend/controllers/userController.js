70ff407549c3206ac7f576977a35739fa109b035
Wesley <wesaboo@gmail.com>
2020-11-03 20:59:18 -0800

    req token should return user data, not workign

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,5 +23,17 @@ export const authUser = asyncHandler(async (req, res) => {
 
 // Get user profile
 export const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("GOT HERE");
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,9 +11,13 @@ export const protect = expressAsyncHandler(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log(decoded);
+      req.user = await User.findById(decoded.id).select("-password");
       next();
-    } catch (error) {}
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("No token found, not authorized");
+    }
   }
 
   if (!token) {
