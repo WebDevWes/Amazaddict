ec178975e7481266c2c34af4879a53fd00c943b1
Wesley <wesaboo@gmail.com>
2020-11-05 17:39:23 -0800

    adds bcrypt authentication for new user, created login reducer&action

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,6 +23,38 @@ export const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Create new user
+// POST /api/users
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already has an account");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
 // Get user profile
 // GET /api/users/profile
 
