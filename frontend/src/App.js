b20efd7727ebf3df626afb54fb684768160b2d3d
Wesley <wesaboo@gmail.com>
2020-11-10 15:45:26 -0800

    created register page, action, constant

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import ProductPage from "./pages/ProductPage";
 import Cart from "./pages/Cart";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/product/:id" component={ProductPage} />
           <Route path="/cart/:id?" component={Cart} />
           <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
         </Container>
       </div>
       <Footer />
