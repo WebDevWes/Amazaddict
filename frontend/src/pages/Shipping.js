87f3a0d489053d97a8d6f714adc9a51362caf4cd
Wesley <wesaboo@gmail.com>
2020-11-12 15:34:47 -0800

    create checkout status bar

--- frontend/src/pages/Shipping.js
+++ frontend/src/pages/Shipping.js
@@ -3,6 +3,7 @@ import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
 import { shippingAddress as saveAdress } from "../actions/cartActions";
+import CheckoutSteps from "../components/checkoutSteps";
 
 export default function Shipping(props) {
   const cart = useSelector((state) => state.cart);
@@ -22,6 +23,7 @@ export default function Shipping(props) {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
