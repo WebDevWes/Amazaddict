3a18af45d6920cac4d9f657d10ccd6e20bfd759d
Wesley <wesaboo@gmail.com>
2020-11-16 16:06:32 -0800

    created backend order routes and controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,35 @@
+import asyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
+
+// Create new order , POST /api/orders
+export const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    orderPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No item in your order");
+    return;
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      orderPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+    res.status(201).json({ createdOrder });
+  }
+});
