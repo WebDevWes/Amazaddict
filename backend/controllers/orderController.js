125b78fc9e6bf071ecec4b148e34ce48f381cc3a
Wesley <wesaboo@gmail.com>
2020-11-18 02:51:21 -0800

    add get route and controller for grabbing order by id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -33,3 +33,17 @@ export const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json({ createdOrder });
   }
 });
+
+// Get an order by ID , GET /api/orders/:id
+export const getOrderItems = asyncHandler(async (req, res) => {
+  const order = await (await Order.findById(req.params.id)).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { addOrderItems } from "../controllers/orderController.js";
+import {
+  addOrderItems,
+  getOrderItems,
+} from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").post(protect, addOrderItems);
+router.route("/:id").get(protect, getOrderItems);
 
 export default router;
