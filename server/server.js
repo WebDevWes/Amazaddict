0a85f28276eef109ce8b0f015aa6d7b13f9d2ea4
Wesley <wesaboo@gmail.com>
2021-07-22 16:18:04 -0700

    adds atlas cluster connectionn

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/connection.js'
 import products from './data/products.js'
 
 dotenv.config() || 3001
+
+connectDB()
 
 const app = express()
 
