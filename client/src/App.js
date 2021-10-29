51e115dded2bac17f19feb437ccfe6ac3a987869
Wesley <wesaboo@gmail.com>
2021-10-28 18:35:04 -0700

    finsih login screen, adds to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
+import Login from './pages/Login'
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <main>
         <Container>
           <Route path='/' exact component={Home} />
+          <Route path='/login' component={Login} />
           <Route path='/product/:id' component={Product} />
           <Route path='/cart/:id?' component={Cart} />
         </Container>
