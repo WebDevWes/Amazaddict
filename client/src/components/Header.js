10c5550a4adeec1420ed7953a3d77c6e33a32815
Wesley <wesaboo@gmail.com>
2021-11-02 17:53:08 -0700

    adds navbar adjustment for logging in and out, adds useraction for logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import { logout } from '../redux/actions/userActions'
 
 export default function Header() {
+  const {
+    userLogin: { userData },
+  } = useSelector((state) => state)
+
+  const dispatch = useDispatch()
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
       <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
@@ -21,12 +33,23 @@ export default function Header() {
                   Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <i className='fas fa-user nav-icon' />
-                  Sign In
-                </Nav.Link>
-              </LinkContainer>
+              {userData ? (
+                <NavDropdown title={userData.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <LinkContainer to='/login'>
+                  <Nav.Link>
+                    <i className='fas fa-user nav-icon' />
+                    Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
